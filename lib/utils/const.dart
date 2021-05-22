3d365c23b48d9f0c34d708dfe0ccda03184afa1d
Ahmad Faaiz Al-auza'i <afaaiz2@gmail.com>
2021-05-22 21:39:39 +0700

    Change base url

--- lib/utils/const.dart
+++ lib/utils/const.dart
@@ -1,4 +1,4 @@
-const url = 'https://api-ferma.herokuapp.com';
+const url = 'http://31.220.62.156:3000';
 const weatherUrl = 'https://api.weatherapi.com/v1';
 
 const weatherKey = 'ee226016714c4d77a9e05454202011';
